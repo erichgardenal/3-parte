af0d5f2efcb0338b839b7b85807eed453a316240
erichgardenal <129407539+erichgardenal@users.noreply.github.com>
2023-09-27 11:41:10 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-document.querySelector('.tecla_pom');
+// Pom-----------------------------------------
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+ListaDeTeclas[0].onclick = tocaSomPom;
