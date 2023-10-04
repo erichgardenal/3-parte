69a630044d1c86f2cc51f60b5d6ffe7d93762622
erichgardenal <129407539+erichgardenal@users.noreply.github.com>
2023-10-04 11:39:43 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-// Pom-----------------------------------------
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSomPom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const ListaDeTeclas = document.querySelectorAll('.tecla');
 
-ListaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+//enquanto
+while (contador < ListaDeTeclas.length) {
+    const tecla = ListaDeTeclas[contador]
+    const instrumento = tecla.classlist[1];
+    //template tring
+    constidAudio = '#som_${instrumento}';
+    console.log(idAudio);
+
+    ListaDeTeclas[contador].onclick = function () {
+        tocaSomPom('#som_tecla_pom');
+    }
+
+contador = contador + 1;
+
+console.log(contador);
+
+}
+
